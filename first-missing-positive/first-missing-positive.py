408d1c00b3a448c20f359b2f838f7cca2f0147ce
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-16 18:51:06 +0530

    Time: 373 ms (77.05%) | Memory: 34.9 MB (10.34%) - LeetSync

--- first-missing-positive/first-missing-positive.py
+++ first-missing-positive/first-missing-positive.py
@@ -1,7 +1,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         st = set(nums)
-        i =1
+        i = 1
         while i in st:
             i +=1
         return i    
