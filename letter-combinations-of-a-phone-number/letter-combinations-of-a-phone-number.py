9ed2f39f64723b1bf1583163db318c5d6677008c
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-16 18:50:06 +0530

    Time: 43 ms (24.14%) | Memory: 16.4 MB (12.20%) - LeetSync

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,9 +1,9 @@
 from itertools import product
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits=="":
+        if digits =="":
             return []
-        mp = {
+        phone_map = {
             "2":"abc",
             "3":"def",
             "4":"ghi",
@@ -12,20 +12,16 @@ class Solution:
             "7":"pqrs",
             "8":"tuv",
             "9":"wxyz"
-
         }
-        temp_l = []
-        for d in digits:
-            temp_l.append(mp[d])
-        prod = product(*temp_l)
+        temp_list = []
+        for i in digits:
+            temp_list.append(phone_map[i])
+        prod =   product(*temp_list)  
         result = []
         for p in list(prod):
             if p =="":
                 continue
             s = "".join(p)    
-            result.append(s)    
-        
-        return result
-
-
+            result.append(s)
+        return result    
         
