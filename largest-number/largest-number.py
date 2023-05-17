00170ee9bac57976f7073874489fa63bd0d9c4c9
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-17 15:57:42 +0530

    Time: 62 ms (18.21%) | Memory: 16.2 MB (27.36%) - LeetSync

--- largest-number/largest-number.py
+++ largest-number/largest-number.py
@@ -1,15 +1,15 @@
 import functools
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        def comparator(s1, s2):
-            if int(s1+s2) < int(s2+s1):
-                return -1
-            if int(s1+s2) > int(s2+s1):
+        def compare(n1,n2):
+            if int(n1+n2)>int(n2+n1):
                 return 1
+            elif int(n1+n2)<int(n2+n1):
+                return -1
             return 0
-        
-        nums = [str(num) for num in nums]
-        nums = sorted(nums, key = functools.cmp_to_key(comparator),  reverse = True)
-        ans = '0' if nums[0] == '0' else ''.join(nums)
-        return ans          
+        nums = [str(n) for n in nums]
+        nums = sorted(nums,key= functools.cmp_to_key(compare),reverse=True)
+        ans = '0' if nums[0]=='0' else ''.join(nums)
+        return ans    
+
         
