918c4c7f0587f8b379abd3adba97e3109f2cde8b
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-17 19:34:05 +0530

    Time: 41 ms (63.21%) | Memory: 16.2 MB (15.49%) - LeetSync

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -8,7 +8,8 @@ class Solution:
                 return mid
             elif mid*mid<x:
                 ans = mid
-                l = mid+1
+                l =mid+1  
             else:
                 r = mid-1
-        return ans                
+        return ans              
+        
