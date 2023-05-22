4cdd7407352ad5d31ae7099a0ee43197914b41ba
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-22 16:52:42 +0530

    Time: 303 ms (72.06%) | Memory: 18.7 MB (59.34%) - LeetSync

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -3,33 +3,32 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         no_island = 0
-        for i in range(n):
-            for j in range(m):
+        for i in range(0,n):
+            for j in range(0,m):
                 if grid[i][j]=="1":
+                    grid[i][j] = "0"
                     no_island +=1
-                    grid[i][j]="0"
                     qu = []
                     qu.append([i,j])
                     while qu:
                         front = qu[0]
-                        row = front[0]
+                        row= front[0]
                         col = front[1]
                         qu.pop(0)
                         if row-1>=0 and grid[row-1][col]=="1":
+                            grid[row-1][col]= "0"
                             qu.append([row-1,col])
-                            grid[row-1][col] = "0"
                         if row+1<n and grid[row+1][col]=="1":
+                            grid[row+1][col]= "0"
                             qu.append([row+1,col])
-                            grid[row+1][col] = "0"
                         if col-1>=0 and grid[row][col-1]=="1":
+                            grid[row][col-1]= "0"
                             qu.append([row,col-1])
-                            grid[row][col-1] = "0"
                         if col+1<m and grid[row][col+1]=="1":
-                            qu.append([row,col+1])
-                            grid[row][col+1] = "0"  
-        return no_island                              
+                            grid[row][col+1]= "0"
+                            qu.append([row,col+1]) 
+        return no_island                           
 
 
-
-
+        
         
