8f6d4fe8e792d6c3650502bcca58c6ef5d5a2dc8
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-23 15:20:59 +0530

    Time: 189 ms (57.41%) | Memory: 17.9 MB (15.82%) - LeetSync

---
+++ non-decreasing-array/non-decreasing-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        if len(nums)==1:
+            return True
+        flag = 0
+
+        for i in range(len(nums)-1):
+            if nums[i]<=nums[i+1]:
+                continue
+            if flag:
+                return False
+            if i==0 or nums[i+1]>=nums[i-1]:
+                nums[i]=nums[i+1]
+            else:
+                nums[i+1]=nums[i]
+            flag = 1
+        return True                   
+        
