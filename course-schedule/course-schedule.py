1fbd0f7496f5dfa5f6a8cac99faf3e1a62b7e446
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-23 12:49:02 +0530

    Time: 116 ms (31.72%) | Memory: 19.3 MB (16.73%) - LeetSync

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -6,20 +6,22 @@ class Solution:
 
         vis = set()
         def dfs(crs):
-            if preMap[crs]==[]:
-                return True
             if crs in vis:
                 return False
-            vis.add(crs)    
-
+            if preMap[crs]==[]:
+                return True
+            vis.add(crs)
             for pre in preMap[crs]:
                 if not dfs(pre):
                     return False
-            vis.remove(crs)
-            preMap[crs]=[]
-            return True
+            vis.remove(crs)        
+            preMap[crs] = []        
+            return True 
 
         for c in range(numCourses):
             if not dfs(c):
                 return False
-        return True                        
+        return True        
+
+
+        
