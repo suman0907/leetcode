efe0a61e5a80524dde6299f915c9ff150c01daac
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-24 21:15:42 +0530

    Time: 187 ms (66.91%) | Memory: 17.8 MB (16.48%) - LeetSync

--- non-decreasing-array/non-decreasing-array.py
+++ non-decreasing-array/non-decreasing-array.py
@@ -6,10 +6,13 @@ class Solution:
                 continue
             if flag:
                 return False
-            if i==0 or nums[i+1]>=nums[i-1]:
-                nums[i]= nums[i+1]  
-            else:
-                nums[i+1]= nums[i]
+            if i==0 or nums[i+1]>nums[i-1]:
+                nums[i]=nums[i+1]
+            elif nums[i+1]<nums[i-1]:
+                nums[i+1]=nums[i]
             flag = 1
-        return True                  
+        return True            
+
+
+
         
