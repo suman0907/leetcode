30ef3cfbb9fd575e2e4da9943e6d4d8c91985cc3
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-25 17:21:48 +0530

    Time: 285 ms (54.16%) | Memory: 18.1 MB (24.43%) - LeetSync

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -6,25 +6,25 @@ class Solution:
         atl = set()
         def dfs(r,c,vis,preheight):
             if (r,c) in vis or r<0 or c<0 or r==rows or c==cols or heights[r][c]<preheight:
-                return 
+                return
             vis.add((r,c))
-            dfs(r-1,c,vis,heights[r][c])
+            dfs(r-1,c,vis,heights[r][c])  
             dfs(r+1,c,vis,heights[r][c])
             dfs(r,c-1,vis,heights[r][c])
-            dfs(r,c+1,vis,heights[r][c])    
-
+            dfs(r,c+1,vis,heights[r][c])  
 
         for c in range(cols):
             dfs(0,c,pac,heights[0][c])
             dfs(rows-1,c,atl,heights[rows-1][c])
-
         for r in range(rows):
             dfs(r,0,pac,heights[r][0])
-            dfs(r,cols-1,atl,heights[r][cols-1]) 
+            dfs(r,cols-1,atl,heights[r][cols-1])
 
         result = []
         for r in range(rows):
             for c in range(cols):
-                if (r,c) in pac and (r,c)  in atl:
+                if (r,c) in pac and (r,c) in atl:
                     result.append([r,c])
-        return result            
+        return result                
+
+        
