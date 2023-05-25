49bc78e1c452a612ab601a46f6cc1059aa43eee7
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-25 22:09:41 +0530

    Time: 177 ms (47.29%) | Memory: 17.5 MB (39.22%) - LeetSync

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -3,13 +3,11 @@ class Solution:
         price = [float("inf")]*n
         price[src]=0
         for i in range(k+1):
-            tempPrice =  price.copy()
+            tempPrice = price.copy()
             for s,d,p in flights:
-                if price[s]==float("inf"):
-                    continue
-                if price[s]+p < tempPrice[d]:
-                    tempPrice[d]= price[s]+p
-
-            price = tempPrice             
-        return -1 if price[dst]==float("inf") else price[dst]
+                if price[s]+p<tempPrice[d]:
+                    tempPrice[d]=price[s]+p
+            price = tempPrice
+        return -1 if price[dst]==float("inf") else price[dst]           
+            
         
