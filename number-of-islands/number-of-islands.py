443c6e8a3dcf602512e732a44c740cbbb2ac932d
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-25 12:37:09 +0530

    Time: 294 ms (81.61%) | Memory: 18.6 MB (62.01%) - LeetSync

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -3,32 +3,29 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         no_island = 0
-        for i in range(0,n):
-            for j in range(0,m):
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]=="1":
-                    grid[i][j] = "0"
-                    no_island +=1
-                    qu = []
-                    qu.append([i,j])
+                    no_island += 1
+                    grid[i][j]= "0"
+                    qu=[(i,j)]
                     while qu:
-                        front = qu[0]
-                        row= front[0]
-                        col = front[1]
+                        row = qu[0][0]
+                        col = qu[0][1]
                         qu.pop(0)
                         if row-1>=0 and grid[row-1][col]=="1":
-                            grid[row-1][col]= "0"
-                            qu.append([row-1,col])
+                            grid[row-1][col]="0"
+                            qu.append((row-1,col))
                         if row+1<n and grid[row+1][col]=="1":
-                            grid[row+1][col]= "0"
-                            qu.append([row+1,col])
+                            grid[row+1][col]="0"
+                            qu.append((row+1,col))
                         if col-1>=0 and grid[row][col-1]=="1":
-                            grid[row][col-1]= "0"
-                            qu.append([row,col-1])
+                            grid[row][col-1]="0"
+                            qu.append((row,col-1))
                         if col+1<m and grid[row][col+1]=="1":
-                            grid[row][col+1]= "0"
-                            qu.append([row,col+1]) 
-        return no_island                           
+                            grid[row][col+1]="0"
+                            qu.append((row,col+1)) 
+        return no_island                               
 
 
-        
         
