44e97c11f582beb6e2b81cda92aa3c59379e43f0
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-25 12:13:16 +0530

    Time: 572 ms (50.75%) | Memory: 16.7 MB (53.00%) - LeetSync

--- island-perimeter/island-perimeter.py
+++ island-perimeter/island-perimeter.py
@@ -3,18 +3,16 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         perimeter = 0
-        for i in range(0,n):
-            for j in range(0,m):
-                perimeter += 4*grid[i][j]
+        for i in range(n):
+            for j in range(m):
+                perimeter +=4*grid[i][j]
                 if i>0:
-                    perimeter -= grid[i][j]*grid[i-1][j]
+                    perimeter -=grid[i][j]*grid[i-1][j]
                 if i<n-1:
-                    perimeter -= grid[i][j]*grid[i+1][j]
+                    perimeter -=grid[i][j]*grid[i+1][j]
                 if j>0:
-                    perimeter -= grid[i][j]*grid[i][j-1]
+                    perimeter -= grid[i][j]*grid[i][j-1]  
                 if j<m-1:
-                    perimeter -= grid[i][j]*grid[i][j+1] 
-        return perimeter                       
-
-
+                    perimeter -=grid[i][j]*grid[i][j+1]
+        return perimeter                     
         
