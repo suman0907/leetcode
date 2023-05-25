0d43d74001a44bea081211f5244f140354e2f919
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-25 14:48:14 +0530

    Time: 124 ms (93.80%) | Memory: 16.5 MB (74.07%) - LeetSync

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -1,38 +1,35 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = len(grid[0])
+        m= len(grid[0])
         max_area = 0
-        no_island = 0
-        for i in range(0,n):
-            for j in range(0,m):
+        for  i in range(n):
+            for j in range(m):
                 if grid[i][j]==1:
-                    no_island +=1
                     grid[i][j]=0
-                    queue = []
-                    queue.append([i,j])
-                    area = 1
-                    
-                    while queue:
-                        front = queue[0]
-                        queue.pop(0)
-                        row = front[0]
+                    qu =[(i,j)]
+                    curr_area = 1
+                    while qu:
+                        front = qu[0]
+                        row= front[0]
                         col = front[1]
+                        qu.pop(0)
                         if row-1>=0 and grid[row-1][col]==1:
                             grid[row-1][col]=0
-                            queue.append([row-1,col])
-                            area +=1
+                            qu.append((row-1,col))
+                            curr_area +=1
                         if row+1<n and grid[row+1][col]==1:
                             grid[row+1][col]=0
-                            queue.append([row+1,col])
-                            area +=1
+                            qu.append((row+1,col))
+                            curr_area +=1
                         if col-1>=0 and grid[row][col-1]==1:
                             grid[row][col-1]=0
-                            queue.append([row,col-1]) 
-                            area +=1
+                            qu.append((row,col-1))
+                            curr_area +=1
                         if col+1<m and grid[row][col+1]==1:
                             grid[row][col+1]=0
-                            queue.append([row,col+1])  
-                            area +=1 
-                    max_area = max(max_area,area)        
-        return max_area                          
+                            qu.append((row,col+1))
+                            curr_area +=1 
+                    max_area = max(max_area,curr_area)
+        return max_area                               
+        
