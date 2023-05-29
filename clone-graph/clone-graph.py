fbc1aa4036257e7a7133bf9bee465dcdf75a439c
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-29 14:37:09 +0530

    Time: 48 ms (26.36%) | Memory: 16.7 MB (15.07%) - LeetSync

--- clone-graph/clone-graph.py
+++ clone-graph/clone-graph.py
@@ -9,14 +9,14 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         oldToNew = {}
-        
         def dfs(node):
             if node in oldToNew:
                 return oldToNew[node]
             copy = Node(node.val)
-            oldToNew[node] = copy
+            oldToNew[node]=copy
             for nei in node.neighbors:
                 copy.neighbors.append(dfs(nei))
             return copy
-        return dfs(node) if node else None           
+        return dfs(node) if node else None        
+            
         
