7ce2e70826b0db49865176c021dce236c12d6f18
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-30 12:40:37 +0530

    Time: 3164 ms (48.08%) | Memory: 120.8 MB (6.72%) - LeetSync

--- count-sub-islands/count-sub-islands.py
+++ count-sub-islands/count-sub-islands.py
@@ -6,17 +6,16 @@ class Solution:
         def dfs(r,c):
             if r<0 or c<0 or r==rows or c==cols or (r,c) in vis or grid2[r][c]==0:
                 return True
-            vis.add((r,c))
-            res = True
+            vis.add((r,c))    
+            res= True
             if grid1[r][c]==0:
-                res = False
+                res= False
             res= dfs(r+1,c) and res
             res= dfs(r-1,c) and res
-            res= dfs(r,c+1) and res
             res= dfs(r,c-1) and res
-            return res
+            res= dfs(r,c+1) and res
+            return res        
 
-            
 
         count = 0
         for r in range(rows):
