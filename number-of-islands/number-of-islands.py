a0631409c9028f8130acec84a13be4e81422c795
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-30 12:23:04 +0530

    Time: 291 ms (86.15%) | Memory: 18.7 MB (63.48%) - LeetSync

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -6,26 +6,23 @@ class Solution:
         for i in range(n):
             for j in range(m):
                 if grid[i][j]=="1":
-                    no_island += 1
+                    no_island +=1
                     grid[i][j]= "0"
-                    qu=[(i,j)]
+                    qu = [(i,j)]
                     while qu:
-                        row = qu[0][0]
-                        col = qu[0][1]
-                        qu.pop(0)
+                        front = qu.pop(0)
+                        row, col = front[0], front[1]
                         if row-1>=0 and grid[row-1][col]=="1":
-                            grid[row-1][col]="0"
                             qu.append((row-1,col))
+                            grid[row-1][col]="0"
                         if row+1<n and grid[row+1][col]=="1":
-                            grid[row+1][col]="0"
                             qu.append((row+1,col))
+                            grid[row+1][col]="0"
                         if col-1>=0 and grid[row][col-1]=="1":
-                            grid[row][col-1]="0"
                             qu.append((row,col-1))
+                            grid[row][col-1]="0"
                         if col+1<m and grid[row][col+1]=="1":
-                            grid[row][col+1]="0"
-                            qu.append((row,col+1)) 
-        return no_island                               
-
-
+                            qu.append((row,col+1))
+                            grid[row][col+1]="0"   
+        return no_island                             
         
