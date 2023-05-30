e99940dcf28ddd472398df7411d7ed439c5be9bc
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-30 12:33:59 +0530

    Time: 134 ms (81.48%) | Memory: 16.5 MB (76.67%) - LeetSync

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -1,35 +1,33 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m= len(grid[0])
+        m = len(grid[0])
         max_area = 0
-        for  i in range(n):
+        for i in range(n):
             for j in range(m):
                 if grid[i][j]==1:
-                    grid[i][j]=0
-                    qu =[(i,j)]
                     curr_area = 1
+                    grid[i][j]=0
+                    qu = [(i,j)]
                     while qu:
-                        front = qu[0]
-                        row= front[0]
-                        col = front[1]
-                        qu.pop(0)
+                        front = qu.pop(0)
+                        row,col = front[0],front[1]
                         if row-1>=0 and grid[row-1][col]==1:
-                            grid[row-1][col]=0
                             qu.append((row-1,col))
+                            grid[row-1][col]=0
                             curr_area +=1
                         if row+1<n and grid[row+1][col]==1:
-                            grid[row+1][col]=0
                             qu.append((row+1,col))
+                            grid[row+1][col]=0
                             curr_area +=1
                         if col-1>=0 and grid[row][col-1]==1:
-                            grid[row][col-1]=0
                             qu.append((row,col-1))
+                            grid[row][col-1]=0
                             curr_area +=1
                         if col+1<m and grid[row][col+1]==1:
-                            grid[row][col+1]=0
                             qu.append((row,col+1))
-                            curr_area +=1 
-                    max_area = max(max_area,curr_area)
-        return max_area                               
+                            grid[row][col+1]=0 
+                            curr_area +=1
+                    max_area = max(max_area,curr_area)  
+        return max_area                             
         
