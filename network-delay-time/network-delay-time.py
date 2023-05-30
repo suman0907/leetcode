2d841b6983c053f1f6a0801edf083d844b376a1b
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-05-30 19:19:19 +0530

    Time: 502 ms (25.40%) | Memory: 18.3 MB (54.42%) - LeetSync

--- network-delay-time/network-delay-time.py
+++ network-delay-time/network-delay-time.py
@@ -1,25 +1,27 @@
-from collections import defaultdict
 import heapq
+from collections import defaultdict
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        mp = collections.defaultdict(list)
-        for s,d,t in times:
-            mp[s].append([d,t])
-        heap = []    
+        mp = defaultdict(list)
+        for s,d, t in times:
+            mp[s].append([t,d])
+        heap = []
         heapq.heappush(heap,[0,k])
         total_time = 0
-        vis = set()
-        while heap:
+        vis  = set()
+        while heap: 
             front = heapq.heappop(heap)
-            src = front[1]
             time = front[0]
+            src = front[1]
             if src in vis:
                 continue
             vis.add(src)
-            for dst,t in mp[src]:
-                if dst not in vis:
-                    heapq.heappush(heap,[t+time,dst])
-            total_time= max(total_time,time) 
-        return total_time  if len(vis)==n else -1    
+            for t,d in mp[src]:
+                if d not in vis:
+                    heapq.heappush(heap,[t+time,d]) 
+            total_time = max(total_time,time)
+        return total_time if len(vis)==n else -1             
 
 
+
+        
