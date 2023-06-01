06dbe5603cbddd62c70bdbb461876023f42457a4
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-06-01 18:19:03 +0530

    Time: 131 ms (86.84%) | Memory: 16.4 MB (85.13%) - LeetSync

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -6,28 +6,29 @@ class Solution:
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==1:
-                    curr_area = 1
                     grid[i][j]=0
+                    curr_area = 1
                     qu = [(i,j)]
                     while qu:
                         front = qu.pop(0)
-                        row,col = front[0],front[1]
+                        row = front[0]
+                        col = front[1]
                         if row-1>=0 and grid[row-1][col]==1:
-                            qu.append((row-1,col))
                             grid[row-1][col]=0
+                            qu.append((row-1,col))
                             curr_area +=1
                         if row+1<n and grid[row+1][col]==1:
-                            qu.append((row+1,col))
                             grid[row+1][col]=0
+                            qu.append((row+1,col))
                             curr_area +=1
                         if col-1>=0 and grid[row][col-1]==1:
-                            qu.append((row,col-1))
                             grid[row][col-1]=0
+                            qu.append((row,col-1))
                             curr_area +=1
                         if col+1<m and grid[row][col+1]==1:
+                            grid[row][col+1]=0
                             qu.append((row,col+1))
-                            grid[row][col+1]=0 
-                            curr_area +=1
-                    max_area = max(max_area,curr_area)  
-        return max_area                             
+                            curr_area +=1 
+                    max_area = max(max_area,curr_area) 
+        return max_area                              
         
