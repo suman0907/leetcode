5ccbe4558a625abd7d78e171307581c9630d31ff
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-06-01 17:56:26 +0530

    Time: 62 ms (5.24%) | Memory: 16.4 MB (32.83%) - LeetSync

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,10 +1,10 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        order_map = {order[i]:i for i in range(len(order))}
+        order_map = {order[i]: i for i in range(len(order))}
         prev = [order_map[i] for i in words[0]]
-        for i in range(1,len(words)):
-            curr = [order_map[w] for w in words[i]]
-            if curr<prev:
+        for word in words[1:]:
+            curr = [order_map[i] for i in word]
+            if prev>curr:
                 return False
             prev = curr
         return True        
