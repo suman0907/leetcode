38ea991fcf1c43ce7b6252b3a648a51d92118711
suman0907 <33617415+suman0907@users.noreply.github.com>
2023-06-01 18:31:30 +0530

    Time: 292 ms (43.15%) | Memory: 18.2 MB (18.23%) - LeetSync

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,27 +2,29 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows = len(heights)
         cols = len(heights[0])
-        pac= set()
+        pac = set()
         atl = set()
         def dfs(r,c,vis,preheight):
             if r<0 or c<0 or r==rows or c==cols or (r,c) in vis or heights[r][c]<preheight:
                 return False
             vis.add((r,c))
-            dfs(r+1,c,vis,heights[r][c])
             dfs(r-1,c,vis,heights[r][c]) 
-            dfs(r,c+1,vis,heights[r][c]) 
-            dfs(r,c-1,vis,heights[r][c])     
-
+            dfs(r+1,c,vis,heights[r][c]) 
+            dfs(r,c-1,vis,heights[r][c]) 
+            dfs(r,c+1,vis,heights[r][c])    
+            
         for c in range(cols):
             dfs(0,c,pac,heights[0][c])
             dfs(rows-1,c,atl,heights[rows-1][c])
         for r in range(rows):
             dfs(r,0,pac,heights[r][0])
-            dfs(r,cols-1,atl,heights[r][cols-1])
-        result = []  
-        for r in range(rows):
+            dfs(r,cols-1,atl,heights[r][cols-1]) 
+
+        result = []
+        for  r in range(rows):
             for c in range(cols):
                 if (r,c) in pac and (r,c) in atl:
                     result.append([r,c])
-        return result                  
+        return result                      
+
         
