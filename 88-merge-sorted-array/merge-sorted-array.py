b9a35f01633cfb3627dc06f64688fd2b9b561537
suman0907 <33617415+suman0907@users.noreply.github.com>
2024-02-20 18:40:03 +0530

    Time: 35 ms (85.76%) | Memory: 16.6 MB (61.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,20 +5,20 @@ class Solution:
         """
         i = m-1
         j= n-1
-        k = m+n-1
+        k = n+m-1
         while i>=0 and j>=0:
             if nums1[i]>=nums2[j]:
-                nums1[k]=nums1[i]
-                i -=1
+                nums1[k]= nums1[i]
                 k -=1
+                i  -=1
             else:
-                nums1[k]=nums2[j]
-                j-=1
-                k-=1
+                nums1[k]= nums2[j]
+                k -=1
+                j  -=1
         while j>=0:
-            nums1[k]=nums2[j]
-            j -=1
+            nums1[k]= nums2[j]
             k -=1
+            j  -=1
 
 
 
