a7d8c12b1ec9c5edd912024fd25a795bc2b3fba6
suman0907 <33617415+suman0907@users.noreply.github.com>
2024-03-02 19:57:41 +0530

    Time: 44 ms (49.97%) | Memory: 16.8 MB (51.56%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        mp1 ={}
+        mp2 = {}
+        for i in range(len(s)):
+            x = s[i]
+            y = t[i]
+            if (x in mp1 and mp1[x]!=y) or (y in mp2 and mp2[y]!=x):
+                return False
+            mp1[x]=y
+            mp2[y]=x
+        return True    
+        
