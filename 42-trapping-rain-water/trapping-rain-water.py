f1301a8a1994b73067f333fca7215421bde0237f
suman0907 <33617415+suman0907@users.noreply.github.com>
2024-04-02 19:22:54 +0530

    Time: 117 ms (20.40%) | Memory: 18.6 MB (39.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,15 +1,14 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        la = [0]*len(height)
-        ra = [0]*(len(height))
-        la[0] = height[0]
-        ra[-1] = height[-1]
+        arr1 = [0]*len(height)
+        arr2 = [0]*len(height)
+        arr1[0]=height[0]
+        arr2[-1]=height[-1]
         for i in range(1,len(height)):
-            la[i]= max(la[i-1],height[i])
+            arr1[i]= max(arr1[i-1],height[i])
         for i in range(len(height)-2,-1,-1):
-            ra[i]= max(ra[i+1],height[i]) 
+            arr2[i]=max(arr2[i+1],height[i])
         water = 0
         for i in range(len(height)):
-            water +=abs(min(la[i],ra[i])-height[i]) 
-        return water         
-        
+            water+=abs(min(arr1[i],arr2[i])-height[i])
+        return water        
