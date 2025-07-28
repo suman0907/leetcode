ddb4705b8d001b7a5405d9b1d19af3f7c5d73392
suman0907 <33617415+suman0907@users.noreply.github.com>
2025-07-28 23:58:14 +0530

    Time: 21 ms (63.68%) | Memory: 18.6 MB (41.99%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,15 +1,17 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums)==1:
-            return True    
-        jump = 0
+            return True
+        if nums[0]==0:
+            return False
+
         target = len(nums)-1
-        for i in range(len(nums)):
-            jump = max(jump,i+nums[i])
+        jump = 0
+        for i in range(0,len(nums)-1):
+            jump=max(jump,i+nums[i])
             if jump<=i:
                 return False
-            if jump>= target:
+            if jump>=target:
                 return True
-        return False        
-
+        return False            
         
