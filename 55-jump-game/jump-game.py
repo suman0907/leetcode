39895f96c0d9bea74361cc9b1ee533990d36f9ba
suman0907 <33617415+suman0907@users.noreply.github.com>
2025-07-29 19:22:41 +0530

    Time: 29 ms (55.83%) | Memory: 18.7 MB (42.11%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,15 +3,15 @@ class Solution:
         if len(nums)==1:
             return True
         if nums[0]==0:
-            return False
-
+            return False   
+        jump= 0
         target = len(nums)-1
-        jump = 0
-        for i in range(0,len(nums)-1):
-            jump=max(jump,i+nums[i])
+        for i in range(len(nums)-1):
+            jump = max(jump, i+nums[i])
             if jump<=i:
                 return False
             if jump>=target:
                 return True
         return False            
+
         
